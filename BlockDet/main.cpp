a51363f621bab1723c93f52a0110d6609c94adbd
duocore <duocore@localhost>
2019-06-14 10:54:25 +0800

    A new project, a application to detect client is blocked

---
+++ BlockDet/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: duocore
+ *
+ * Created on 2019年6月14日, 上午10:52
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
