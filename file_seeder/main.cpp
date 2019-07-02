4105d5bdebe76e5efabd62b4a7f0bd8f3c191d17
duocore <duocore@localhost>
2019-07-02 21:34:17 +0800

    add a new project file_seeder which is based on libtorrent.

---
+++ file_seeder/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * file_seeder project is designed to run on a resource limited linux system with 1GB RAM, 1 core cpu and about a 30GB disk.
+ * - The main perpor is running as a `torrent seeder`
+ * - addtionally it can retrive the configure from a http server and update the current seed task.
+ * So it also must have the ability to download files from the other client and web url.
+ * - to update the tasks delete the old out-time files.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: duocore
+ *
+ * Created on 2019年7月2日, 下午9:23
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
